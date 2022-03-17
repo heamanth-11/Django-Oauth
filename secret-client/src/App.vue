d2155db47b34e18e325db6182400bd235b174eeb
heamanth-11 <heamanthofficial@gmail.com>
2022-03-17 17:32:06 +0530

    initial commit

---
+++ secret-client/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <Oauth/>
+</template>
+
+<script>
+import Oauth from "./components/Oauth.vue"
+export default {
+  name: 'App',
+    components: {
+      Oauth
+   
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ secret-client/src/store/index.js
@@ -0,0 +1,18 @@
+import { createStore } from 'vuex'
+// import axios from 'axios'
+export default createStore({
+  state: {
+  },
+  getters: {
+  },
+  mutations: {
+  },
+  actions: {
+  //  async signin({commit}){
+     
+
+   
+  },
+  modules: {
+  }
+})
